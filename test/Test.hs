b0fb2c9d18f65a2f732af156e20b3d80daf07063
Tenor Biel <tenorbiel@gmail.com>
2014-12-08 22:34:29 +0000

    setup initial tests

--- test/Test.hs
+++ test/Test.hs
@@ -1,8 +1,11 @@
 module Main (main) where
 
 import Test.Hspec
+import Test.QuickCheck
+import Whip
 
 main :: IO ()
 main = hspec $ do
-    it "should be working?" $ do
-        2 `shouldBe` (2 :: Int)
+    describe "Expr" $ do
+        it "should compare equivalent strings" $
+            property $ \s -> String s == String s
